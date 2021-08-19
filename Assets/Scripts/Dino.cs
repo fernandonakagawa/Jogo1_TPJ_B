6e4505cf084674490147da0dfd7570b7d9b50cc3
Fernando Henrique Yoshiaki Nakagawa <fernando.nakagawa@solmarista.org.br>
2021-08-19 17:22:29 -0300

    flipX do personagem

--- Assets/Scripts/Dino.cs
+++ Assets/Scripts/Dino.cs
@@ -11,43 +11,61 @@ public class Dino : MonoBehaviour
     private AudioSource audio;
     public AudioClip audioUi;
     public AudioClip audioMorri;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
     // Start is called before the first frame update
     void Start()
     {
         posicaoInicial = this.transform.position;
         audio = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        //andar para frente ou para trás
+        if (Input.GetKey(KeyCode.D)) Andar(true);
+        if (Input.GetKey(KeyCode.A)) Andar(false);
+
+        if (Input.GetKey(KeyCode.W) && tempoPulado <= 0) Pular();
+        tempoPulado -= Time.deltaTime;
+
+        VerificarMorte();
+    }
+
+    void Andar(bool paraFrente)
+    {
+        if(paraFrente)
         {
+            sprite.flipX = false;
             Vector3 posicao = transform.position;
             posicao.x += velocidade;
             transform.position = posicao;
         }
-        if (Input.GetKey(KeyCode.A))
+        else
         {
+            sprite.flipX = true;
             Vector3 posicao = transform.position;
             posicao.x -= velocidade;
             transform.position = posicao;
         }
-        if (Input.GetKey(KeyCode.W) && tempoPulado <= 0)
-        {
-            Rigidbody2D rb = GetComponent<Rigidbody2D>();
-            Vector2 forca = new Vector2(0f, 15f);
-            rb.AddForce(forca, ForceMode2D.Impulse);
-            tempoPulado = tempoPulo;
-        }
-        //Debug.Log(Time.deltaTime);
-        tempoPulado -= Time.deltaTime;
-        if(this.transform.position.y < -10f)
+    }
+    void Pular()
+    {
+        
+        Vector2 forca = new Vector2(0f, 15f);
+        rb.AddForce(forca, ForceMode2D.Impulse);
+        tempoPulado = tempoPulo;
+    }
+    void VerificarMorte()
+    {
+        if (this.transform.position.y < -10f)
         {
             this.transform.position = posicaoInicial;
             audio.PlayOneShot(audioMorri);
         }
-
     }
 
     void OnCollisionEnter2D(Collision2D collision)

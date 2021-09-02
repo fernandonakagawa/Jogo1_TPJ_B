76796f2b4fee165ad21aec7f7bf85c5c9bf1baf8
Fernando Henrique Yoshiaki Nakagawa <fernando.nakagawa@solmarista.org.br>
2021-09-02 17:23:03 -0300

    Tela de abertura com botão de jogar, animações e música de início

---
+++ Assets/Scripts/Abertura.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Abertura : MonoBehaviour
+{
+    //public AudioClip musica;
+
+    private AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float la = Soma(2, 3);
+        Debug.Log(la);
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+
+    private float Soma(float n1, float n2)
+    {
+        float resposta = n1 + n2;
+        return resposta;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}

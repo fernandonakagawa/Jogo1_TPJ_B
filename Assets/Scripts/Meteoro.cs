2adc31d5bb542522a2b3e99cad98bfe79eaf996d
Fernando Henrique Yoshiaki Nakagawa <fernando.nakagawa@solmarista.org.br>
2021-09-08 16:43:24 -0300

    meteoros perseguidores

---
+++ Assets/Scripts/Meteoro.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteoro : MonoBehaviour
+{
+    public GameObject dino;
+    public float velocidade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 posicaoDino = dino.transform.position;
+        Vector3 posicaoMeteoro = this.transform.position;
+        if (posicaoDino.x > posicaoMeteoro.x) posicaoMeteoro.x += velocidade;
+        else posicaoMeteoro.x -= velocidade;
+
+        if (posicaoDino.y > posicaoMeteoro.y) posicaoMeteoro.y += velocidade;
+        else posicaoMeteoro.y -= velocidade;
+
+        this.transform.position = posicaoMeteoro;
+    }
+}

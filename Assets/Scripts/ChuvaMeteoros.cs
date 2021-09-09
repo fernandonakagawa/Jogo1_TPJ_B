b08d63dff9bda9e42daa787e7a7848f9e45a0eaf
Fernando Henrique Yoshiaki Nakagawa <fernando.nakagawa@solmarista.org.br>
2021-09-09 17:19:20 -0300

    meteoro, explosão, chuva de meteoros

---
+++ Assets/Scripts/ChuvaMeteoros.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChuvaMeteoros : MonoBehaviour
+{
+    public GameObject meteoroPrefab;
+    public GameObject dino;
+    public float meteoroTempo;
+    private float meteoroTempoAtual;
+    void Start()
+    {
+        meteoroTempoAtual = meteoroTempo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meteoroTempoAtual -= Time.deltaTime;
+        if(meteoroTempoAtual <= 0)
+        {
+            Vector3 meteoroPos = dino.transform.position;
+            meteoroPos.y += 10f;
+            meteoroPos.x += Random.Range(-1, 25);
+            //Debug.Log(Random.Range(24, 66));
+            Instantiate(meteoroPrefab, meteoroPos, dino.transform.rotation);
+            meteoroTempoAtual = meteoroTempo;
+        }
+        //Random rand = 
+    }
+}
--- Assets/Scripts/Meteoro.cs
+++ Assets/Scripts/Meteoro.cs
@@ -6,15 +6,18 @@ public class Meteoro : MonoBehaviour
 {
     public GameObject dino;
     public float velocidade;
+    public GameObject explosao;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Rigidbody2D>()
+            .AddForce(new Vector2(-6f, 0), ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         Vector3 posicaoDino = dino.transform.position;
         Vector3 posicaoMeteoro = this.transform.position;
         if (posicaoDino.x > posicaoMeteoro.x) posicaoMeteoro.x += velocidade;
@@ -24,5 +27,14 @@ public class Meteoro : MonoBehaviour
         else posicaoMeteoro.y -= velocidade;
 
         this.transform.position = posicaoMeteoro;
+        */
+
+        if (transform.position.y < -14f) Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Instantiate(explosao, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
 }
